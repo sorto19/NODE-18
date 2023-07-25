2999b26445a9ed7bf3ae5cff9c005ea169d99942
Ramon sorto <ramonsorto1@gmail.com>
2023-07-25 13:45:54 -0600

    trd

--- fs.js
+++ fs.js
@@ -1,6 +1,6 @@
 //fyle-systems usaremos asincrono
 const fs = require( 'node:fs' )// siempre es usar mejor node: antes de el prefijo
-
+const {promisify} = require('node:util') //modulos que no tienen promesas nativas 
 const stats = fs.statSync( './test.txt' )
 console.log(
     stats.isFile(),
@@ -9,5 +9,6 @@ console.log(
     stats.size,
 )
 
-const text = fs.readFileSync( './test.txt', 'utf-8' )
-console.log(text)
+const text = fs.readFileSync( './test.txt', 'utf-8', ( err, text ) => {
+    console.log(text)
+})
